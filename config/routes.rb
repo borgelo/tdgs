e79fa6315d6b6a76acd5c59fe3719dd9f86693cf
Børge Lotre <borge.lotre@gmail.com>
2014-05-26 23:17:54 +0200

    sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Tdgs::Application.routes.draw do
   resources :users
-    match '/signup',  to: 'users#new',            via: 'get'
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :commodities
 
